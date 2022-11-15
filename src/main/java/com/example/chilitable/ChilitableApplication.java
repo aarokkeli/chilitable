48415225ae1571ad6111debe8a99700ac7b74bf3
Aaro Miikkulainen <aaro@aaro-air.lan>
2022-11-15 22:24:46 +0200

    Finished project

--- src/main/java/com/example/chilitable/ChilitableApplication.java
+++ src/main/java/com/example/chilitable/ChilitableApplication.java
@@ -25,7 +25,7 @@ public class ChilitableApplication {
 		SpringApplication.run(ChilitableApplication.class, args);
 	}
 
-	/* @Bean
+	@Bean
 	public CommandLineRunner chiliDemo(ChiliRepository crepo, UserRepository urepo, CategoryRepository caterepo,
 			ColorRepository colorepo) {
 		return (args) -> {
@@ -57,6 +57,12 @@ public class ChilitableApplication {
 			crepo.save(chili1);
 			crepo.save(chili2);
 			crepo.save(chili3);
+			
+			// Create users admin/admin and user/user
+			User user1 = new User("user", "$2a$10$xDSOHxQuLo4syde/Xy9aMOBoUxFVuzTTzbndyL1Jm27G/9qM6rp9m", "USER");
+			User user2 = new User("admin", "$2a$10$6TyLJ3vgERNGGhoWKbzrxOiUF/vlFt5PE62DiF.2qvfEx8UfhwW0u", "ADMIN");
+			urepo.save(user1);
+			urepo.save(user2);
 		}; 
-	} */
+	}
 }
